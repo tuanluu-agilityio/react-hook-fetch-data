662f98bd47c8df191830a02d1c79b0ff8ff29501
Tuan Luu <tuan.luu@asnet.com.vn>
2022-05-05 22:33:33 +0700

    Init project

---
+++ src/App.jsx
@@ -0,0 +1,82 @@
+import React, { 
+  Fragment,
+  useState, 
+  useEffect,
+} from 'react';
+import axios from 'axios';
+
+const URL_API = 'https://hn.algolia.com/api/v1/search';
+
+const useDataApi = (initialUrl, initialData) => {
+  const [data, setData] = useState(initialData);
+  const [url, setUrl] = useState(initialUrl);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsError(false);
+      setIsLoading(true);
+
+      try {
+        const result = await axios(url);
+  
+        setData(result.data);
+      } catch (error) {
+        setIsError(true);        
+      }
+
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, [url]);
+
+  return [{ data, isLoading, isError }, setUrl];
+}
+
+function App() {
+  const [query, setQuery] = useState('redux');
+  const [{ data, isLoading, isError }, doFetch] = useDataApi(
+    `${URL_API}?query=${query}`,
+    { hits: [] }
+  );
+
+  return (
+    <Fragment>
+      <form
+        onSubmit={event => {
+          doFetch(`${URL_API}?query=${query}`);
+
+          event.preventDefault();
+        }}
+      >
+        <input 
+          type="text"
+          value={query}
+          onChange={(e) => setQuery(e.target.value)}
+        />
+        <button type='submit'>Search</button>
+      </form>
+
+      {isError && <div>Something went wrong ...</div>}
+
+      {isLoading ? (
+          <div>Loading...</div>
+        ) : (
+          <ul>
+            {
+              data.hits.map(item => (
+                <li key={item.objectID}>
+                  <a href={item.url}>{item.title}</a>
+                </li>
+              ))
+            }
+          </ul>
+        )
+      }
+    </Fragment>
+  );
+}
+
+export default App;
